ccf2fde8472b9fa79f63b8d7740db09337302751
Chris <uni@hermannc.de>
2017-06-13 13:46:03 +0200

    Make IngredientController class not abstract
    This class used to be abstract for unknown reasons. It should never have
    been abstract in the first place.
    
    Signed-off-by: Chris <uni@hermannc.de>

--- MealPlanner/src/controller/IngredientController.java
+++ MealPlanner/src/controller/IngredientController.java
@@ -1,5 +1,45 @@
 package controller;
 
-public class IngredientController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import model.Ingredient;
+import model.MealPlannerApp;
+import model.Profile;
+import view.IngredientsTab;
+
+public class IngredientController implements Observer, ActionListener {
+	
+	private Ingredient model;
+	private IngredientsTab view;
+	
+	public IngredientController(Ingredient model, IngredientsTab view) {
+		this.model = model;
+		this.view = view;
+		
+		this.model.addObserver(this);
+		
+		this.view.getAddButton().addActionListener(this);
+		this.view.getDeleteButton().addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if (event.getActionCommand() == IngredientsTab.ACTION_COMMAND_ADD_INGREDIENT) {
+			Profile activeProfile = MealPlannerApp.getInstance().getActiveProfile();
+			System.out.println("add");
+			activeProfile.addIngredient(new Ingredient());
+		}
+		if (event.getActionCommand() == IngredientsTab.ACTION_COMMAND_DELETE_INGREDIENT) {
+			
+		}
+	}
+
+	@Override
+	public void update(Observable observable, Object object) {
+		
+	}
 
 }
