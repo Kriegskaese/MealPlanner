0a0f87d635e259e3eb8c6047ce2d6269eb24b3e9
Chris <uni@hermannc.de>
2017-06-14 06:42:28 +0200

    Partially implement the delete button in the IngredientsTab
    Signed-off-by: Chris <uni@hermannc.de>

--- MealPlanner/src/controller/IngredientsTabController.java
+++ MealPlanner/src/controller/IngredientsTabController.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JTable;
+
 import model.Application;
 import model.Ingredient;
 import model.Profile;
@@ -29,17 +31,30 @@ public class IngredientsTabController implements Observer, ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		if (event.getActionCommand() == IngredientsTab.ACTION_COMMAND_ADD_INGREDIENT) {
 			Profile activeProfile = Application.getInstance().getActiveProfile();
-			Ingredient ingredient = new Ingredient();
-			activeProfile.addIngredient(ingredient);
+			activeProfile.addIngredient(new Ingredient());
 		}
 		if (event.getActionCommand() == IngredientsTab.ACTION_COMMAND_DELETE_INGREDIENT) {
-
+			Profile activeProfile = Application.getInstance().getActiveProfile();
+			Ingredient selectedIngredient = getSelectedIngredient(view.getIngredientsTable());
+			activeProfile.removeIngredient(selectedIngredient);
 		}
 	}
 
 	@Override
 	public void update(Observable observable, Object object) {
 
+	}
+	
+	private Ingredient getSelectedIngredient(JTable table) {
+		int selectedIngredientId = table.getSelectedRow();
+		Profile activeProfile = Application.getInstance().getActiveProfile();
+		
+		for (Ingredient ingredient : activeProfile.getIngredients()) {
+			if (ingredient.getId() == selectedIngredientId) {
+				return ingredient;
+			}
+		}
+		return null;
 	}
 
 }
--- MealPlanner/src/model/Profile.java
+++ MealPlanner/src/model/Profile.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Observable;
 
 import database.AddQuery;
+import database.DeleteQuery;
 
 public class Profile extends Observable {
-	
+
 	private String name;
-	
+
 	private List<Ingredient> ingredients = new ArrayList<Ingredient>();
 	private List<IngredientTag> ingredientTags = new ArrayList<IngredientTag>();
 	private List<IngredientCategory> ingredientCategories = new ArrayList<IngredientCategory>();
@@ -17,15 +18,15 @@ public class Profile extends Observable {
 	private List<RecipeTag> recipeTags = new ArrayList<RecipeTag>();
 	private List<RecipeCategory> recipeCategories = new ArrayList<RecipeCategory>();
 	private List<MealPlan> mealPlans = new ArrayList<MealPlan>();
-	
+
 	public Profile(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -35,5 +36,17 @@ public class Profile extends Observable {
 		new AddQuery("ingredients", ingredients.size());
 		setChanged();
 		notifyObservers();
+	}
+
+	public void removeIngredient(Ingredient ingredient) {
+		int ingredientId = ingredient.getId();
+		ingredients.remove(ingredient);
+		new DeleteQuery("ingredients", ingredientId);
+		setChanged();
+		notifyObservers();
+	}
+
+	public List<Ingredient> getIngredients() {
+		return ingredients;
 	}
 }
