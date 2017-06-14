a93feefaacbea6f7bdbb15b94e2ddeb9b9c41be4
Chris <uni@hermannc.de>
2017-06-14 16:35:00 +0200

    Add constructor to RecipeEntry class
    Signed-off-by: Chris <uni@hermannc.de>

--- MealPlanner/src/model/RecipeEntry.java
+++ MealPlanner/src/model/RecipeEntry.java
@@ -5,6 +5,11 @@ public class RecipeEntry {
 	private int amount;
 	private Ingredient ingredient;
 
+	public RecipeEntry(int amount, Ingredient ingredient) {
+		this.amount = amount;
+		this.ingredient = ingredient;
+	}
+
 	public int getAmount() {
 		return amount;
 	}
@@ -20,23 +25,23 @@ public class RecipeEntry {
 	public void setIngredient(Ingredient ingredient) {
 		this.ingredient = ingredient;
 	}
-	
+
 	public int getPrice() {
 		return amount * ingredient.getPriceInCentsPer100();
 	}
-	
+
 	public int getEnergy() {
 		return amount * ingredient.getEnergyPer100();
 	}
-	
+
 	public int getProtein() {
 		return amount * ingredient.getProteinPer100();
 	}
-	
+
 	public int getFat() {
 		return amount * ingredient.getFatPer100();
 	}
-	
+
 	public int getCarbs() {
 		return amount * ingredient.getCarbsPer100();
 	}
