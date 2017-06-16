08fb3c9bee4c4933443f7ab53a611601c3d0c08c
Chris <uni@hermannc.de>
2017-06-16 08:48:26 +0200

    Add ModifyButton class
    Signed-off-by: Chris <uni@hermannc.de>

---
+++ MealPlanner/src/view/ModifyButton.java
@@ -0,0 +1,11 @@
+package view;
+
+public class ModifyButton extends Button {
+
+	private static final long serialVersionUID = 1L;
+
+	public ModifyButton(String actionCommand) {
+		super(actionCommand, "Modify");
+	}
+
+}
