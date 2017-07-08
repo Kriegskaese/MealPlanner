2fcca2e431eea12c63ad5f1a59261b4a1ea33a18
Chris <uni@hermannc.de>
2017-07-08 23:29:33 +0200

    Improve readability of test.
    Signed-off-by: Chris <uni@hermannc.de>

--- MealPlanner/testsrc/model/Ingredient_AddPropertyName.java
+++ MealPlanner/testsrc/model/Ingredient_AddPropertyName.java
@@ -9,16 +9,15 @@ public class Ingredient_AddPropertyName {
 	@Test
 	public void addTwoPropertyNames() {
 		Ingredient ingredient = new Ingredient(1);
+
 		ingredient.addPropertyName("Protein");
 		ingredient.addPropertyName("Calcium");
 
-		boolean result = false;
+		assertTrue(propertyNamesContains(ingredient, "Protein") && propertyNamesContains(ingredient, "Calcium"));
+	}
 
-		if (ingredient.getpropertyNames().contains("Protein") && ingredient.getpropertyNames().contains("Calcium")) {
-			result = true;
-		}
-
-		assertTrue(result);
+	private boolean propertyNamesContains(Ingredient ingredient, String string) {
+		return ingredient.getpropertyNames().contains(string);
 	}
 
 }
